56776d835b6974c8bc90b31c45708e7262df5928
demoo3 demo2 <gulayeminova56@gmail.com>
2024-09-06 20:54:43 +0000

    demo

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 "use strict";
 $(document).ready(function () {
 
+    $('#me').click(function (e) {
+        e.preventDefault();
+        let a = (b = "") => {
+            if (!$.trim($('#inputme').val()) == '') {
+                let vall = $('#inputme').val()
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class="line"> </div>')
+                    , bigDiv = $(`<div class="message">${b}</div>`)
+                $('newH3').append(vall);
+                $('newDiv').append(newH3);
+                $('bigDiv').append(newDiv);
+                return bigDiv
+            }
+            else {
+                return
+            }
+        }
+        $('#messageme').append(a('class="end"'));
+        $('#inputme').val("");
+    });
 });
