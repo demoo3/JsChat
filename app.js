ffc723c9bd27b7f96229c205de4ae5bd9a55bf55
demoo3 demo2 <gulayeminova56@gmail.com>
2024-09-07 13:22:56 +0000

    demo

--- app.js
+++ app.js
@@ -2,31 +2,28 @@
 $(document).ready(function () {
 
     $('#me').click(function (e) {
-        e.preventDefault();
-        if (!$.trim($('#inputme').val()) == '') {
-            $('.end').append($('#inputme').val());
+        e.preventDefault()
+        function a(b='') {
+            if (!$.trim($('#inputme').val()) == '') {
+                let vall = $('#inputme').val()
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class="line"> </div>')
+                    , bigDiv = $(`<div class="end">${b}</div>`)
+                $('newH3').append(vall)
+                $('newDiv').append(newH3)
+                $('bigDiv').append(newDiv)
+                return bigDiv
+            }
+            else {
+                return
+            }
         }
-        else {
-            return
-        }
-        $('#inputme').val('')
-       //let a = b => {
-       //    if (!$.trim($('#inputme').val()) == '') {
-       //        let vall = $('#inputme').val()
-       //            , newH3 = $('<h3> </h3>')
-       //            , newDiv = $('<div class="line"> </div>')
-       //            , bigDiv = $(`<div class="end">${b}</div>`)
-       //        $('newH3').append(vall);
-       //        $('newDiv').append(newH3);
-       //        $('bigDiv').append(newDiv);
-       //        return bigDiv
-       //    }
-       //    else {
-       //        return
-       //    }
-       //}
-       ////$('#messageyou').append(a());
-       //$('#messageme').append(a($('#inputme').val()));
-       //$('#inputme').val("");
+        $('#messageyou').append(a());
+        $('#messageme').append(a($('#inputme').val()))
+        $('#inputme').val("")
+
     });
+
 });
+
+
