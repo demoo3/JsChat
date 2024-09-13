62d566d73ef581b81698c94ae4c32644ecbd878d
demoo3 demo2 <gulayeminova56@gmail.com>
2024-09-13 16:32:45 +0000

    demo

--- app.js
+++ app.js
@@ -1,67 +1,58 @@
 "use strict";
 $(document).ready(function () {
 
-    $('#me').click(function (e) {
-        e.preventDefault()
-        let a = b => {
-            if (!$.trim($('#inputme').val()) == '') {
-                $('.end').append(`<h3>${b}</h3>`);
+    $("#me").click(function (e) {
+        e.preventDefault();
+        function myel(myclass = "") {
+            if (!$.trim($("#inputme").val()) == "") {
+                let vall = $("#inputme").val()
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class="line"> </div>')
+                    , bigDiv = $(`<div ${myclass} > </div>`)
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiv)
+                return bigDiv
             }
             else {
                 return
             }
         }
-        //$('#messageyou').append(a());
-        $('#messageme').append(a($('#inputme').val()));
-        $('#inputme').val("")
+        $("#messageyou").append(myel())
+        $("#messageme").append(myel('class= "end"'))
+        $("#inputme").val("")
+
         document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+
     });
-    $('#you').click(function (e) {
-        e.preventDefault()
-        let a = b => {
-            if (!$.trim($('#inputyou').val()) == '') {
-                $('.end').append(`<h3>${b}</h3>`);
+
+    $("#you").click(function (e) {
+        e.preventDefault();
+        function myel(myclass = "") {
+            if (!$.trim($("#inputyou").val()) == "") {
+                let vall = $("#inputyou").val()
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class="line"> </div>')
+                    , bigDiv = $(`<div ${myclass} > </div>`)
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiv)
+                return bigDiv
             }
             else {
                 return
             }
         }
-        //$('#messageyou').append(a());
-        $('#messageyou').append(a($('#inputyou').val()))
-        $('#inputyou').val("")
-        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+        $("#messageme").append(myel())
+        $("#messageyou").append(myel('class= "end"'))
+        $("#inputyou").val("")
+
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageme").scrollHeight)
+
     });
-    //
-    //$('#me').click(function (e) {
-    //    e.preventDefault()
-    //    function a(b){
-    //        if (!$.trim($('#inputme').val()) == '') {
-    //            let vall = $('#inputme').val()
-    //                , newH3 = $('<h3> </h3>')
-    //                , newDiv = $('<div class="line"> </div>')
-    //                , bigDiv = $(`<div class="end" ${b}> </div>`)
-    //            $('newH3').append(vall)
-    //            $('newDiv').append(newH3)
-    //            $('bigDiv').append(newDiv)
-    //            return bigDiv
-    //        }
-    //        else {
-    //            return
-    //        }
-    //    }
-    //    //$('#messageyou').append(a());
-    //    $('#messageme').append(a('class="end"'))
-    //    $('#inputme').val("")
-    //
-    //});
+
+
+
 });
-
-//let vall = $('#inputme').val()
-//    , newH3 = $('<h3> </h3>')
-//    , newDiv = $('<div class="line"> </div>')
-//    , bigDiv = $(`<div class="end" ${b}> </div>`)
-//$('newH3').append(vall)
-//$('newDiv').append(newH3)
-//$('bigDiv').append(newDiv)
-//return bigDiv
-
