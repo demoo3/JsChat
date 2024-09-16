a0684ad90bf7408fb86c8241e38f7442ed5b32dc
demoo3 demo2 <gulayeminova56@gmail.com>
2024-09-16 04:45:54 +0000

    demo

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ $(document).ready(function () {
         $("#messageyou").append(myel('class= "end"'))
         $("#inputyou").val("")
 
-        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
-        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageme").scrollHeight)
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
 
     });
 
